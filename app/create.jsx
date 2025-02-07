e3c280f1eb04dab10244ccb00d6b557d3eb647a4
3liantte <eliantte49@gmail.com>
2025-02-07 14:32:43 +0200

    React Expo Floating Navbar

---
+++ app/create.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ColorList from '../components/ColorList'
+
+const create = () => {
+  return (
+    <View>
+      <ColorList color="#00a478"/>
+    </View>
+  )
+}
+
+export default create
---
+++ app/explore.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ColorList from '../components/ColorList'
+
+const explore = () => {
+  return (
+    <View>
+      <ColorList color="#c7313d"/>
+    </View>
+  )
+}
+
+export default explore
